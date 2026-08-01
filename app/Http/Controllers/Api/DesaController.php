96840ed687dbf11139af1cc248f532764c094763
zoentach <zoentachweb@gmail.com>
2026-08-01 15:36:54 +0700

    update kode kecamatan pada DesaController

--- app/Http/Controllers/Api/DesaController.php
+++ app/Http/Controllers/Api/DesaController.php
@@ -30,10 +30,20 @@ class DesaController extends Controller
     private function getNamaKecamatan($kode)
     {
         $daftarKecamatan = [
-            '127101' => 'Sipirok',
-            '127102' => 'Angkola Timur',
-            '127103' => 'Arse',
-            '127104' => 'Saipar Dolok Hole'
+            '120301' => 'ANGKOLA BARAT',
+            '120302' => 'BATANG TORU',
+            '120303' => 'ANGKOLA TIMUR',
+            '120304' => 'SIPIROK',
+            '120305' => 'SAIPAR DOLOK HOLE',
+            '120306' => 'ANGKOLA SELATAN',
+            '120307' => 'BATANG ANGKOLA',
+            '120314' => 'ARSE',
+            '120320' => 'MARANCAR',
+            '120321' => 'SAYURMATINGGI',
+            '120322' => 'AEK BILAH',
+            '120329' => 'MUARA BATANGTORU',
+            '120330' => 'ANGKOLA SANGKUNUR',
+            '120332' => 'ANGKOLA MUARATAIS',
         ];
 
         return $daftarKecamatan[$kode] ?? 'Kecamatan Tidak Diketahui';
